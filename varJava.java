f0c3343d89675f9ba0e114671b82f347a44bb793
vonclick <56036543+vonclick@users.noreply.github.com>
2019-10-14 16:09:53 +0200

    Create varJava.java

---
+++ varJava.java
@@ -0,0 +1,17 @@
+public class varJava {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  movieName = "Indiana Jones and the Last Crusade";
+		boolean isSeen    = true;
+		int     year      = 1989;
+		float  rate       = 8.2f;
+		
+		System.out.println(movieName);	   
+		System.out.println(isSeen);		   
+		System.out.println(year);		   
+		System.out.println(rate);		   
+	}
+
+}
